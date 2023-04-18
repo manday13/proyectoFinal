791c7ec89fd6894a9642aac09a5d85c5df1849eb
Adrià Manday <manday1993@gmail.com>
2023-04-18 19:29:06 +0200

    about v0.0

--- client/src/components/about/Mission.jsx
+++ client/src/components/about/Mission.jsx
@@ -1,9 +1,14 @@
-
+import './about.css'
 
 function Mission() {
     return (
         <>
-            <h1>Mission</h1>
+            <div className='ab'>
+                <h1 className='bout'>Our Mission</h1>
+                <p>At ||Website Name||, our mission is to empower and support women who have been released from prison to successfully reintegrate into society. We believe that every woman deserves a second chance and that by providing free workshops and therapy/mental health services, we can help them overcome challenges, build essential life skills, and thrive in their communities. Our goal is to break down barriers, reduce recidivism rates, and promote positive change in the lives of formerly incarcerated women, creating a brighter future for them and their families.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, necessitatibus quidem eos sed at maiores itaque eveniet laborum debitis nulla, inventore impedit voluptatum cumque magnam repellat praesentium molestiae. Rerum fuga corporis non veritatis tempore recusandae maiores quasi quis tenetur delectus, magnam atque aut rem! Deleniti non debitis nemo hic repellat. Enim dicta aliquam, corrupti rem nostrum maiores neque nam ducimus quae hic voluptatibus aperiam libero esse obcaecati dignissimos deleniti veniam atque expedita officiis! Beatae, cum eius odit non nam maxime iste corrupti fuga dicta sint debitis voluptas voluptatem aut neque! Quod porro nobis voluptatem labore dicta accusamus eveniet qui fugit cum commodi, minus deserunt debitis esse velit soluta quas facilis quia! Ab iste facilis est excepturi quas inventore. Sequi, ipsum quis! Id pariatur odit doloremque nulla at, perferendis dolor itaque.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quasi non repudiandae officia id, maxime nobis quidem vero eligendi eius consequuntur error aliquid, quaerat officiis aspernatur dolorem sequi odio ab. Ab deserunt praesentium saepe exercitationem magni perspiciatis ipsa animi illum, sint, quibusdam non vel a. Facilis ab recusandae sequi, vel blanditiis maxime nulla beatae? Provident saepe magnam eligendi ea tempora debitis magni est qui voluptatem perferendis. Cupiditate ratione porro doloremque dicta officia magnam ex? Illum ut facilis sed atque rerum placeat vel molestiae totam natus non vitae quae dolorem, incidunt cum! Voluptatibus et, rerum harum sit modi at quasi beatae distinctio, minus reiciendis nisi omnis. Libero assumenda delectus expedita iste esse reprehenderit, blanditiis quia sit nesciunt rem eveniet quidem a magni sunt aliquid, aliquam eligendi saepe perspiciatis laborum sed dicta!</p>
+            </div>
         </>
     )
 }
