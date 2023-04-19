d42c441e451346d87208f0d7fbafdb23b09bc541
maiaaaam <oumaimaessamadi@gmail.com>
2023-04-19 16:00:16 +0200

    new changes to the front, navbar and some styling

--- client/src/Menu.jsx
+++ client/src/Menu.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Menu.css'
 
 function Menu() {
     return (
--- client/src/components/Sign.jsx
+++ client/src/components/Sign.jsx
@@ -1,11 +1,95 @@
-
+import React, { useState } from "react";
+import "./Sign.css";
 
 function Sign() {
+    const [signIn, setSignIn] = useState(true);
+    const [isClient, setIsClient] = useState(false);
+
+    const toggleForm = () => {
+        setSignIn(!signIn);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // handle sign in form submission
+    };
+
+    const handleSignUpSubmit = (e) => {
+        e.preventDefault();
+        // handle sign up form submission
+    };
+
+    const handleClientCheckboxChange = (e) => {
+        setIsClient(e.target.checked);
+    };
+
     return (
-        <>
-            <h1>Sign</h1>
-        </>
-    )
+        <div>
+            {signIn ? (
+                <form onSubmit={handleSubmit}>
+                    <h2>Sign In</h2>
+                    <label>
+                        Email
+                        <input type="email" name="email" required />
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" name="password" required />
+                    </label>
+                    <button type="submit">Sign In</button>
+                    <p className="sign">
+                        Don't have an account?{" "}
+                        <button type="button" onClick={toggleForm}>
+                            Sign Up
+                        </button>
+                    </p>
+                </form>
+            ) : (
+                <form onSubmit={handleSignUpSubmit}>
+                    <h2>Sign Up</h2>
+                    <label>
+                        Email
+                        <input type="email" name="email" required />
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" name="password" required />
+                    </label>
+                    <label>
+                        Validate Password
+                        <input type="password" name="validatePassword" required />
+                    </label>
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="isClient"
+                            checked={isClient}
+                            onChange={handleClientCheckboxChange}
+                        />
+                        Are you a client?
+                    </label>
+                    {!isClient && (
+                        <label>
+                            Choose one of the following:
+                            <select name="choice" required>
+                                <option value=""></option>
+                                <option value="option1">Tutor</option>
+                                <option value="option2">Artist</option>
+                                <option value="option3">Therapist</option>
+                            </select>
+                        </label>
+                    )}
+                    <button type="submit">Sign Up</button>
+                    <p className="sign">
+                        Already have an account?{" "}
+                        <button type="button" onClick={toggleForm}>
+                            Sign In
+                        </button>
+                    </p>
+                </form>
+            )}
+        </div>
+    );
 }
 
 export default Sign;
