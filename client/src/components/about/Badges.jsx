e78b4d8f934e1f4302342e4b832e41797a213e33
Adrià Manday <manday1993@gmail.com>
2023-04-19 19:38:39 +0200

    about updated

--- client/src/components/about/Badges.jsx
+++ client/src/components/about/Badges.jsx
@@ -1,19 +1,159 @@
+import { useState } from 'react';
+import { Button, Card, Modal } from 'react-bootstrap';
+
 import './about.css'
+import './Badges.css'
 
 function Badges() {
 
-    /* TODO
-        -fetch al back para coger la info de badges
-        -mostrarlos en cards? */
-    
+    const [showModal, setShowModal] = useState(false); // estado para controlar si se muestra o no el modal
+    const [selectedBadge, setSelectedBadge] = useState({}); // estado para almacenar la información del badge seleccionado
+
+    const toggleModal = (badge) => { // función que se encarga de abrir/cerrar el modal y establecer el badge seleccionado
+        setSelectedBadge(badge);
+        setShowModal(!showModal);
+    };
+
+    const badges = [
+        {
+            id: 1,
+            nombre: 'asertividad',
+            foto: 'asertividad.png',
+            descripcion: 'Capacidad para expresarse y defender los propios derechos sin vulnerar los de los demás.'
+        },
+        {
+            id: 2,
+            nombre: 'asistencia',
+            foto: 'asistencia.png',
+            descripcion: 'Capacidad para proporcionar una presencia activa regular y sostenida'
+        },
+        {
+            id: 3,
+            nombre: 'comunicación',
+            foto: 'comunicacion.png',
+            descripcion: 'La capacidad de relacionarse con los demás, de transmitir un mensaje a alguien de un modo adecuado a la persona y a la circunstancias'
+        },
+        {
+            id: 4,
+            nombre: 'fiabilidad',
+            foto: 'fiabilidad.png',
+            descripcion: 'Capacidad para inspirar confianza en los demás a través de las acciones'
+        },
+        {
+            id: 5,
+            nombre: 'adaptabilidad',
+            foto: 'adaptabilidad.png',
+            descripcion: 'Capacidad para aceptar e integrar los cambios y adaptarse a ellos a las personas y al medio ambiente de forma positiva'
+        },
+        {
+            id: 6,
+            nombre: 'asertividad',
+            foto: 'asertividad.png',
+            descripcion: 'Capacidad para expresarse y defender los propios derechos sin vulnerar los de los demás.'
+        },
+        {
+            id: 7,
+            nombre: 'asistencia',
+            foto: 'asistencia.png',
+            descripcion: 'Capacidad para proporcionar una presencia activa regular y sostenida'
+        },
+        {
+            id: 8,
+            nombre: 'comunicación',
+            foto: 'comunicacion.png',
+            descripcion: 'La capacidad de relacionarse con los demás, de transmitir un mensaje a alguien de un modo adecuado a la persona y a la circunstancias'
+        },
+        {
+            id: 9,
+            nombre: 'fiabilidad',
+            foto: 'fiabilidad.png',
+            descripcion: 'Capacidad para inspirar confianza en los demás a través de las acciones'
+        },
+        {
+            id: 10,
+            nombre: 'adaptabilidad',
+            foto: 'adaptabilidad.png',
+            descripcion: 'Capacidad para aceptar e integrar los cambios y adaptarse a ellos a las personas y al medio ambiente de forma positiva'
+        }
+    ];
+
+    // const badgesCards = badges.map((item, idx) => {
+    //     return (
+    //         <Card id={idx} bg="light" className="badges" style={{ width: '18rem' }}>
+    //             <Card.Img variant="top" src="https://placekitten.com/200/300" />
+    //             <Card.Body>
+    //                 <Card.Title>{item.nombre}</Card.Title>
+    //                 {/* <Card.Text>{item.descripcion}</Card.Text> */}
+    //                 <Button variant="primary">+ info</Button>
+    //             </Card.Body>
+    //         </Card>
+    //     )
+    // });
+
+    // return (
+    //     <>
+    //         <h1 className='titleAboutB'>Our Badges</h1>
+    //         <div className='infoAboutB'>
+    //             <p>Alongside the services we provide, we offer various badges upon the completion of the workshops and therapy services. Here is a list of our badges:</p>
+    //         </div>
+    //         <div className="bCards">
+    //             {badgesCards}
+    //         </div>
+    //     </>
+    // )
+
+
+    const badgesCards = badges.map((item) => {
+        return (
+            <Card
+                key={item.id}
+                bg="light"
+                className="badges"
+                style={{ width: '18rem' }}
+            >
+                <Card.Img variant="top" src="https://placekitten.com/300/300" />
+                <Card.Body>
+                    <Card.Title>{item.nombre}</Card.Title>
+                    <Button variant="primary" onClick={() => toggleModal(item)}>
+                        + info
+                    </Button>
+                </Card.Body>
+            </Card>
+        );
+    });
+
     return (
         <>
-            <div className='ab'>
-                <h1 className='bout'>Our Badges</h1>
-                <p>Alongside the services we provide, we offer various badges upon the completion of the workshops and therapy services. Here is a list of our badges:</p>
+            <h1 className="titleAboutB">Our Badges</h1>
+            <div className="infoAboutB">
+                <p>
+                    Alongside the services we provide, we offer various badges upon the
+                    completion of the workshops and therapy services. Here is a list of
+                    our badges:
+                </p>
             </div>
+            <div className="bCards">{badgesCards}</div>
+
+            {/* Componente Modal de Bootstrap */}
+            <Modal show={showModal} onHide={() => toggleModal({})}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{selectedBadge.nombre}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <img
+                        // src={`ruta-a-la-imagen/${selectedBadge.foto}`}
+                        src={"https://placekitten.com/100/100"}
+                        alt={selectedBadge.nombre}
+                    />
+                    <p>{selectedBadge.descripcion}</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => toggleModal({})}>Close</Button>
+                </Modal.Footer>
+            </Modal>
         </>
-    )
+    );
 }
+
 
 export default Badges;
--- client/src/components/about/Story.jsx
+++ client/src/components/about/Story.jsx
@@ -1,10 +1,11 @@
 import './about.css'
+import './Story.css'
 
 function Story() {
     return (
         <>
-            <div className='ab'>
-                <h1 className='bout'>Our Story</h1>
+            <h1 className='titleAboutS'>Our Story</h1>
+            <div className='infoAbout'>
                 <p>||Website Name|| was founded with a deep passion for social justice and a desire to address the unique challenges that women face upon re-entry into society after incarceration. Our team is composed of individuals who have personal experience with the criminal justice system, and we have seen firsthand the struggles that women often encounter in navigating the complexities of reintegration. We believe that every woman deserves access to support and resources to help her rebuild her life, and that's why ||Website Name|| was created.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, necessitatibus quidem eos sed at maiores itaque eveniet laborum debitis nulla, inventore impedit voluptatum cumque magnam repellat praesentium molestiae. Rerum fuga corporis non veritatis tempore recusandae maiores quasi quis tenetur delectus, magnam atque aut rem! Deleniti non debitis nemo hic repellat. Enim dicta aliquam, corrupti rem nostrum maiores neque nam ducimus quae hic voluptatibus aperiam libero esse obcaecati dignissimos deleniti veniam atque expedita officiis! Beatae, cum eius odit non nam maxime iste corrupti fuga dicta sint debitis voluptas voluptatem aut neque! Quod porro nobis voluptatem labore dicta accusamus eveniet qui fugit cum commodi, minus deserunt debitis esse velit soluta quas facilis quia! Ab iste facilis est excepturi quas inventore. Sequi, ipsum quis! Id pariatur odit doloremque nulla at, perferendis dolor itaque.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quasi non repudiandae officia id, maxime nobis quidem vero eligendi eius consequuntur error aliquid, quaerat officiis aspernatur dolorem sequi odio ab. Ab deserunt praesentium saepe exercitationem magni perspiciatis ipsa animi illum, sint, quibusdam non vel a. Facilis ab recusandae sequi, vel blanditiis maxime nulla beatae? Provident saepe magnam eligendi ea tempora debitis magni est qui voluptatem perferendis. Cupiditate ratione porro doloremque dicta officia magnam ex? Illum ut facilis sed atque rerum placeat vel molestiae totam natus non vitae quae dolorem, incidunt cum! Voluptatibus et, rerum harum sit modi at quasi beatae distinctio, minus reiciendis nisi omnis. Libero assumenda delectus expedita iste esse reprehenderit, blanditiis quia sit nesciunt rem eveniet quidem a magni sunt aliquid, aliquam eligendi saepe perspiciatis laborum sed dicta!</p>
